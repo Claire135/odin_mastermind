614865dff33b6a816b83db0e843311bb68c687e5
Claire135 <186298887+Claire135@users.noreply.github.com>
2025-02-24 12:43:53 +0100

    rules_UI added

--- lib/displayable.rb
+++ lib/displayable.rb
@@ -1,14 +1,25 @@
+# Generates player board
+# Controls UI
+
 require_relative 'game_settings'
+require_relative 'colorable'
 
 module Displayable
+
+  def rules_UI
+    puts "You have 8 guesses to crack the code. Grey pegs mean you got the color and position right, white means you didn't."
+  end
   
   def valid_colors_UI
     valid_colors_string = GameSettings::VALID_COLORS.join(", ")
     puts "Valid colours: #{valid_colors_string}"
   end
 
+  def win_UI
+    puts 'Well done you cracked the code!'
+  end
+
+  def lose_UI
+    puts "You'd never be hired by the secret services. Do better next time."
+  end
 end
-
-include Displayable
-
-valid_colors_UI
